d2f314be09cc5579187275e058953dea38aa23a6
Alexey Rozhnov <rozhnov.alexey@gmail.com>
2021-08-11 18:49:32 +0300

    c

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -1,41 +1,38 @@
 from rest_framework import serializers
-
+from .models import StockProduct, Product, Stock
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
-
+    class Meta:
+        model = Product
+        fields = "__all__"
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
-
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
-
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
-        positions = validated_data.pop('positions')
-
-        # создаем склад по его параметрам
+        products_data = validated_data.pop('positions')
         stock = super().create(validated_data)
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for product_data in products_data:
+            StockProduct.objects.create(stock=stock, **product_data)
         return stock
+
+    class Meta:
+        model = Stock
+        fields = "__all__"
 
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
-        positions = validated_data.pop('positions')
+        products_data = validated_data.pop('positions')
 
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-
+        for product_data in products_data:
+            StockProduct.objects.update_or_create(stock=stock, **product_data)
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
